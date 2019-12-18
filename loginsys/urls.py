7b982e808dcbab6be1f3dfca0ed81aa3e130c016
Alexandra Sannikova <sannikovaal@mail.ru>
2019-12-18 03:02:27 +0300

    Register

--- loginsys/urls.py
+++ loginsys/urls.py
@@ -3,5 +3,6 @@ from loginsys import views
 
 urlpatterns = [
     re_path('^login/', views.login),
-    re_path('^logout/', views.logout)
+    re_path('^logout/', views.logout),
+    re_path('^register/', views.register)
 ]
--- loginsys/views.py
+++ loginsys/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
 from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 
 def login(request):
     args = {} # словарь с переменными для шаблона
@@ -22,3 +23,20 @@ def login(request):
 def logout(request):
     auth.logout(request) # деавторизировать
     return redirect('/')
+
+def register(request):
+    args = {} # словарь с переменными для шаблона
+    args.update(csrf(request))
+    args['form'] = UserCreationForm() #создаем пустую форму
+    if(request.POST): # отправлены ли данные из адресной строки или формы регистрации пользователя
+        newuser_form = UserCreationForm(request.POST) # тоже новая форма, но с данными из запроса (с username, password1 и 2)
+        if newuser_form.is_valid():
+            print('valid')
+            newuser_form.save() # т к встроенная форма наследуется от ModellForm, достаточно вызвать save(), чтобы сохр сразу в БД
+            # данные username и password из формы, получаем юзера и логинимся
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password2'])
+            auth.login(request, newuser) # войти, создать сессию для пользователя
+            return redirect('/')
+        else:
+            args['form'] = newuser_form # т к после проверки на валидноть, могут находиться ошибки в форме, добавленные автоматич
+    return render(request, 'register.html', args) # возвращаем форму, когда у нас нет запроса
